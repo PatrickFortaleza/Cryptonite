cc2ebb7de678c7d625e7d5fc06681849192f4e4c
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-03-29 15:59:41 -0700

    Upload example data for a user

---
+++ exampleData/User.ts
@@ -0,0 +1,28 @@
+import { User } from '../schemas/User'
+
+const userData: User = {
+  email: "exampleEmail@gmail.com",
+  password: "$2y$12$ITzE2KZRSM/qtWGU64OQKeYA1tf0J7Hrc9y3k.GFG4EsvUTlfwGTa",
+  transactions: [{
+    coinId: "6e09279c-ad54-4ac2-888d-55f5c4e36f99",
+    numberOfCoins: 2,
+    marketValuePerCoin: 11,
+    dateOfPurchase: 1617058328
+  }, 
+  {
+    coinId: "890449e9-7b73-475c-b9f3-5bb50eb41968",
+    numberOfCoins: 2,
+    marketValuePerCoin: 22,
+    dateOfPurchase: 1617058378
+  },
+  {
+    coinId: "6e09279c-ad54-4ac2-888d-55f5c4e36f99",
+    numberOfCoins: -1,
+    marketValuePerCoin: 11,
+    dateOfPurchase: 1617058378
+  }],
+  cash: 49945,
+  bookValue: 55
+}
+
+console.log(userData)
--- schemas/User.ts
+++ schemas/User.ts
@@ -1,9 +1,9 @@
-import { Transactions } from "./Transactions"
+import { Transaction } from "./Transaction"
 
 export interface User {
   email: string,
   password: string,
-  transactions: Transactions,
+  transactions: Array<Transaction>,
   bookValue: number,
   cash: number, // Default 50,0000
 }
