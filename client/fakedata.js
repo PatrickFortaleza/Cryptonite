cc7f285a9edc4ee28c158efcbc04b397766c58ef
apurugganan <apurugganan1@my.bcit.ca>
2021-03-30 19:15:18 -0700

    Modified fakedata for testing

--- client/fakedata.js
+++ client/fakedata.js
@@ -2,19 +2,29 @@ const countries = [
   
   
   { name: "Bitcoin", 
-    description: "Oss!",
-    imageURL: "https://logos-world.net/wp-content/uploads/2020/08/Bitcoin-Emblem.png" 
+    price: "25.00",
+    oneDay: "+1.0",
+    sevenDay: "+3.0",
+    imageURL: "https://logos-world.net/wp-content/uploads/2020/08/Bitcoin-Emblem.png" ,
+    chart : "Placeholder Chart"
+
   },
 
   
     { name: "Ethereum", 
-    description: "Oui",
-    imageURL: "https://1000logos.net/wp-content/uploads/2018/04/Ethereum-Logo.png"
+    price: "10.00",
+    oneDay: "-2.0",
+    sevenDay: "-4.0",
+    imageURL: "https://1000logos.net/wp-content/uploads/2018/04/Ethereum-Logo.png",
+    chart : "Placeholder Chart"
     },
 
     { name: "Dogge", 
-    description: "🍺Beerrrr",
-    imageURL: "https://flyingatom.shop/img/cms/28_0000_dogecoin-1-z.png"
+    price: "5.00",
+    oneDay: "+3.0",
+    sevenDay: "+6.0",
+    imageURL: "https://flyingatom.shop/img/cms/28_0000_dogecoin-1-z.png",
+    chart : "Placeholder Chart"
     },
 
    
