f80caed3ef18be499d3a033f4066f3396aad2a14
apurugganan <apurugganan1@my.bcit.ca>
2021-03-30 17:25:27 -0700

    Added ListScreen and DetailScreen

--- client/App.js
+++ client/App.js
@@ -1,37 +1,63 @@
-import React from "react";
-import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import Login from "./src/screens/Login.js";
-import Register from "./src/screens/Register.js";
-import Amplify from "aws-amplify";
-import config from "./AWSconfig.json";
-import { AuthProvider } from "./src/context/AuthContext";
-import Navigation from "./src/navigation";
-import { NavigationContainer } from "@react-navigation/native";
+// import React from "react";
+// import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+// import Login from "./src/screens/Login.js";
+// import Register from "./src/screens/Register.js";
+// import Amplify from "aws-amplify";
+// import config from "./AWSconfig.json";
+// import { AuthProvider } from "./src/context/AuthContext";
+// import Navigation from "./src/navigation";
+// import { NavigationContainer } from "@react-navigation/native";
 
-Amplify.configure({
-  Auth: {
-    mandatorySignId: true,
-    region: config.cognito.REGION,
-    userPoolId: config.cognito.USER_POOL_ID,
-    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
-  },
-});
+// Amplify.configure({
+//   Auth: {
+//     mandatorySignId: true,
+//     region: config.cognito.REGION,
+//     userPoolId: config.cognito.USER_POOL_ID,
+//     userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+//   },
+// });
+
+// export default function App() {
+//   return (
+//     <NavigationContainer styles={styles.container}>
+//       <SafeAreaView style={styles.container}>
+//         <AuthProvider>
+//           <Navigation />
+//         </AuthProvider>
+//       </SafeAreaView>
+//     </NavigationContainer>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: "#191919",
+//   },
+// });
+
+
+
+import React from 'react';
+import { NavigationContainer} from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import List from './src/screens/List'
+import Detail from './src/screens/Detail'
+
+
+const Stack = createStackNavigator()
 
 export default function App() {
   return (
-    <NavigationContainer styles={styles.container}>
-      <SafeAreaView style={styles.container}>
-        <AuthProvider>
-          <Navigation />
-        </AuthProvider>
-      </SafeAreaView>
-    </NavigationContainer>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#191919",
-  },
-});
+   <NavigationContainer>
+     <Stack.Navigator>
+        <Stack.Screen name="Companies" component={List} />
+        <Stack.Screen name="DetailScreen" component={Detail}/>
+     </Stack.Navigator>
+   </NavigationContainer>
+    );
+  
+
+};
