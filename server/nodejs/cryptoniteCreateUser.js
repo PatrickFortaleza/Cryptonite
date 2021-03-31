40be6235c0c80071cbe4fd2c9e2778df6d8fc4d1
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2021-03-31 10:41:53 -0700

    Added the get user lambda function

--- server/nodejs/cryptoniteCreateUser.js
+++ server/nodejs/cryptoniteCreateUser.js
@@ -1,27 +1,29 @@
 const connectToDatabase = require('/opt/nodejs/database');
-const aws = require('aws-sdk');
-const cognito = new aws.CognitoIdentityServiceProvider();
 
-exports.handler = async (event) => {
-    const username = event.user.username;
-    const password = event.user.password;
-    const email = event.user.email;
-        const params = {
-            ClientId: "6gd4pka0plvnbupartrdsfgc7b",
-            Username: username,
-            Password: password,
-        };
-        cognito.signUp(params, function(err, data) {
-          if (err) console.log(err, err.stack); // an error occurred
-          else     console.log(data);           // successful response
-        });
-    const db = await connectToDatabase();
-    const users = db.collection('users');
-    const result = users.insertOne({email: email, username: username, password: password, cash: 50000});
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify("Record inserted"),
-    };
-    return response;
+exports.handler = async (event, context) => {
+  console.log(`Attempted to create user with ${JSON.stringify(event)}`)
+  const DEFAULT_CASH = 50000;
+  if (!event.request.userAttributes.sub){
+    context.done("Improper user object", event);
+    return;
+  }
+  const user = {
+    _id: event.request.userAttributes.sub,
+    email: event.request.userAttributes.email,
+    username: event.userName,
+    cash: DEFAULT_CASH,
+    bookValue: 0,
+    transactions: []
+  }
+
+
+  const db = await connectToDatabase();
+  const users = db.collection('users');
+  try{
+    await users.insertOne(user);
+    context.done(null, event);
+    return;
+  } catch (error){
+    context.done(error, event);
+  }
 };
