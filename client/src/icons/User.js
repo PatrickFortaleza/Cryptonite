12b5fb0845e94434301784f71a2013cd92e0baea
Patrick Fortaleza <patrickfortaleza@gmail.com>
2021-04-01 16:50:45 -0700

    #58 implement sparkline on list items

--- client/src/icons/User.js
+++ client/src/icons/User.js
@@ -5,8 +5,8 @@ export function UserDefault(props) {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="20"
+      height="20"
       viewBox="0 0 24 24"
       {...props}
     >
@@ -24,8 +24,8 @@ export function UserActive(props) {
   return (
     <Svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="20"
+      height="20"
       viewBox="0 0 24 24"
       {...props}
     >
