2158a14b861e6fa92c8d80105593f972bfbb3fdb
Stefan Kuznetsov <skuznetsov2@my.bcit.ca>
2021-04-04 17:06:59 -0700

    Implemented a basic profile mockup

--- client/src/components/profile/Profile/index.js
+++ client/src/components/profile/Profile/index.js
@@ -1,12 +1,50 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function index() {
+import { StyleSheet, Text, View, Image } from "react-native";
+import Icon from 'react-native-vector-icons/FontAwesome5';
+export default function Profile({ user }) {
   return (
-    <View>
-      <Text>Profile Component</Text>
+    <View style={styles.container}>
+      <View style={styles.cardContainer}>
+        <View>
+          <Icon size={32} color="black" name="user-circle" />
+        </View>
+        <View>
+          <Text style={styles.text}>John Doe</Text>
+          <Text style={styles.smallText}>San Francisco, CA</Text>
+        </View>
+      </View>
+      <View style={styles.cardContainer}>
+        <View>
+          <Icon size={32} color="black" name="wallet" />
+        </View>
+        <View>
+          <Text style={styles.text}>Wallet: $50, 000</Text>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    borderColor: '#191919',
+    maxWidth: '100%',
+    margin: 'auto',
+    padding: 20
+  },
+  profileImage: {
+    borderRadius: 200
+  },
+  cardContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    paddingBottom: 20
+  },
+  text: {
+    paddingLeft: 10
+  },
+  smallText: {
+    paddingLeft: 10,
+    fontSize: 10
+  }
+});
