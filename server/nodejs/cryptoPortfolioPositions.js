18da6d1af664937beff61090daeeffef8a683733
deepikaverma710 <deepikaverma087@gmail.com>
2021-04-06 13:10:56 -0700

    Calculate portfolioValue for user #83 #84

--- server/nodejs/cryptoPortfolioPositions.js
+++ server/nodejs/cryptoPortfolioPositions.js
@@ -9,23 +9,24 @@ exports.handler = async (event) => {
     return proxyResponse("User not authorized", 400);
   }
   
+  if (!event.body) {
+    return proxyResponse("Empty body", 400);
+  }
+  
+  let messageData;
+  try {
+    messageData = JSON.parse(event.body);
+  } catch (error) {
+    return proxyResponse("Improper body", 400);
+  }
+
+
   const getCurrentPrice = async (id) => {
     const marketValue = await getPrice(id);
     const price = marketValue.data[id].usd;
     return price
   }
-  const id = 'eos'
- const price = await getCurrentPrice(id)
- console.log(price)
-//   if (!event.body) {
-//     return proxyResponse("Empty body", 400);
-//   }
-//   let messageData;
-//   try {
-//     messageData = JSON.parse(event.body);
-//   } catch (error) {
-//     return proxyResponse("Improper body", 400);
-//   }
+  
 
   const db = await connectToDatabase();
   const user = db.collection("users");
@@ -48,34 +49,34 @@ exports.handler = async (event) => {
         },
       ])
       .toArray();
-
-// const test = (result) => {
-//   return Promise.all(result.map(async (t) => 
-//     {
-//       return{
-//         coin: t._id.coinId,
-//         totalCoins: t.totalCoins,
-//         totalAmount: t.totalAmount,
-//         averagePrice: t.totalAmount / t.totalCoins,
-//         currentPrice: await getCurrentPrice(t._id.coinId),
-//       }
-// }))
-// }
-
-// test(result);
-
-   const positions = result.map((t) => ({
+      
+  const positions = await Promise.all( result.map(async (t) => {
+    const currPrice = await getCurrentPrice(t._id.coinId)
+  return  {
   coin: t._id.coinId,
   totalCoins: t.totalCoins,
   totalAmount: t.totalAmount,
   averagePrice: t.totalAmount / t.totalCoins,
-  // currentPrice: getCurrentPrice(t._id.coinId),
-  currentPrice: 6.20,
-  difference: (t.totalAmount - (t.totalCoins * 6.20))
-}));
-console.log(positions);
+  currentPrice: currPrice,
+  totalCurrValue: t.totalCoins * currPrice,
+  difference: (t.totalAmount - (t.totalCoins * currPrice))
+}}));
     
-  return proxyResponse({positions});
+    const cash = await user.find({_id: sub },{cash : 1, _id: 0}).toArray()
+    userCash = cash[0].cash
+    
+let total = 0
+const totalPositionValue =  positions.map(p =>  {
+  total += p.totalCurrValue
+  return total
+})
+   const currentValueOfCoins = totalPositionValue[totalPositionValue.length-1];
+
+  const portfolioValue = userCash + currentValueOfCoins  
+  console.log(portfolioValue)
+  
+  return proxyResponse({positions, portfolioValue});
+  
   } catch (error) {
     return proxyResponse(error.stack, 500);
   }
