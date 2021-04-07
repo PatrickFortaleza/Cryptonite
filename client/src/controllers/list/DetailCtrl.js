518911f520ca967a4cb696dc950a8c135d78ad72
apurugganan <apurugganan1@my.bcit.ca>
2021-04-07 08:20:35 -0700

    Attempt 2 Watchlist

--- client/src/controllers/list/DetailCtrl.js
+++ client/src/controllers/list/DetailCtrl.js
@@ -14,25 +14,65 @@ export default function DetailCtrl({
   const AuthContext = useAuth();
   const WatchListContext = useWatchList();
   const { isAuthenticated } = AuthContext;
-  const { watchListData, setWatchListData} = WatchListContext;
+  //const { watchListData, setWatchListData} = WatchListContext;
 
-  const [isWatchList, setIsWatchList] = useState(isWatchList);
+  const [isWatchList, setIsWatchList] = useState(false);
+  const [ watchListData, setWatchListData] = useState([1,2,3]);
+
+  const save = async() => {
+    try{
+        await AsyncStorage.setItem("watchListArray", JSON.stringify(watchListData))
+        
+        await AsyncStorage.setItem(crypto.id, JSON.stringify(isWatchList))
+    }catch (err){
+      console.log(err)
+    }
+  }
+
+  const load = async() => {
+    try{
+      let jsonValue = await AsyncStorage.getItem("watchListArray")
+      if(jsonValue != null){
+        setWatchListData(JSON.parse(jsonValue))
+      }
+
+      let isWatch = await AsyncStorage.getItem(crypto.id)
+      console.log(isWatch)
+      if(!isWatch != null){
+        setIsWatchList(isWatch)
+      } else {
+        setIsWatchList(false)
+      }
+    }catch (err){
+      console.log(err)
+    }
+  }
   
   const toggleSwitch = () => {
-    setIsWatchList(previousState => !previousState);
+    
+    setIsWatchList(previousState => !previousState)
+    console.log(isWatchList)
+    const id = crypto.id
+    const updatedArray = [...watchListData,id]
+    setWatchListData(updatedArray)
+    save()
+    console.log(watchListData[4])
+
       //   const watchList_ = await watchListData
       //   const id = crypto.id
       //   const existingData = [watchListData]
       //   const updatedArray = [...watchListData, id]
       //   setWatchListData(updatedArray)
-      //   console.log(existingData)
-      //   console.log(updatedArray)
-      //   console.log(await watchListData)
+
   }
 
   useEffect(()=> {
     setIsWatchList(isWatchList)   
   },[isWatchList])
+
+  useEffect(()=>{
+    load()
+  },[]);
 
   const showBuy = (company) => {
     if (!isAuthenticated) return null;
